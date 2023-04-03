20fa9c15023b2b11aa54fdfd01a9867edd339f2d
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-04-03 10:38:13 +0800

    Implement real-time map location update

--- front-end/lib/order_traking_page.dart
+++ front-end/lib/order_traking_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:location/location.dart';
 
 class OrderTrackingPage extends StatefulWidget {
   const OrderTrackingPage({Key? key}) : super(key: key);
@@ -15,10 +16,41 @@ class OrderTrackingPage extends StatefulWidget {
 class OrderTrackingPageState extends State<OrderTrackingPage> {
   final Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
-  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+  static const LatLng sourceLocation = LatLng(14.6416, 121.0393);
+  static const LatLng destination = LatLng(14.6514, 121.0329);
 
   List<LatLng> polylineCoordinates = [];
+  LocationData? currentLocation;
+
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    location.getLocation().then(
+      (location) {
+        currentLocation = location;
+      },
+    );
+
+    GoogleMapController googleMapController = await _controller.future;
+
+    location.onLocationChanged.listen((newLoc) {
+      currentLocation = newLoc;
+
+      googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(
+              newLoc.latitude!,
+              newLoc.longitude!,
+            ),
+          ),
+        ),
+      );
+
+      setState(() {});
+    });
+  }
+
   void getPolyPoints() async {
     PolylinePoints polylinePoints = PolylinePoints();
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
@@ -38,6 +70,7 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
 
   @override
   void initState() {
+    getCurrentLocation();
     getPolyPoints();
     super.initState();
   }
@@ -51,29 +84,40 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
             style: TextStyle(color: Colors.black, fontSize: 16),
           ),
         ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: sourceLocation,
-            zoom: 14.5,
-          ),
-          polylines: {
-            Polyline(
-              polylineId: const PolylineId("route"),
-              points: polylineCoordinates,
-              color: const Color(0xFF7B61FF),
-              width: 6,
-            ),
-          },
-          markers: {
-            const Marker(
-              markerId: MarkerId("source"),
-              position: sourceLocation,
-            ),
-            const Marker(
-              markerId: MarkerId("destination"),
-              position: destination,
-            ),
-          },
-        ));
+        body: currentLocation == null
+            ? Text("Loading...")
+            : GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                  zoom: 14.5,
+                ),
+                polylines: {
+                  Polyline(
+                    polylineId: const PolylineId("route"),
+                    points: polylineCoordinates,
+                    color: const Color(0xFF7B61FF),
+                    width: 6,
+                  ),
+                },
+                markers: {
+                  Marker(
+                    markerId: const MarkerId("currentLocation"),
+                    position: LatLng(currentLocation!.latitude!,
+                        currentLocation!.longitude!),
+                  ),
+                  const Marker(
+                    markerId: MarkerId("source"),
+                    position: sourceLocation,
+                  ),
+                  const Marker(
+                    markerId: MarkerId("destination"),
+                    position: destination,
+                  ),
+                },
+                onMapCreated: (mapController) {
+                  _controller.complete(mapController);
+                },
+              ));
   }
 }
