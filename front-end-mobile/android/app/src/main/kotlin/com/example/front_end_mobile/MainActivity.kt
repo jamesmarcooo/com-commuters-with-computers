25091af8ba2660dbd79481a9ba40b56d7f603778
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-04-24 14:20:51 +0800

    Initialize front end mobile project

---
+++ front-end-mobile/android/app/src/main/kotlin/com/example/front_end_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.front_end_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- front-end/android/app/src/main/kotlin/com/example/google_mao/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.google_mao
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
