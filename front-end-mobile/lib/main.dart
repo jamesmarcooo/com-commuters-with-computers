6bb09f1116b8b867552d4bd2b5e577e9be1f15f3
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-04-24 15:08:50 +0800

    Integrate login in main file

--- front-end-mobile/lib/main.dart
+++ front-end-mobile/lib/main.dart
@@ -1,20 +1,20 @@
 // @dart=2.9
-// import 'package:front_end_mobile/login/login.dart';
+import 'package:front_end_mobile/login/login.dart';
 // import 'package:front_end_mobile/map/map.dart';
 // import 'package:front_end_mobile/signup/signup.dart';
 // import 'package:front_end_mobile/homepage/homepage.dart';
 import 'package:flutter/material.dart';
-// import 'package:splashscreen/splashscreen.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(new MaterialApp(
-    // debugShowCheckedModeBanner: false,
-    // routes: {
-    //   '/login': (context) => Login(),
-    //   '/register': (context) => Signup(),
-    //   '/homepage': (context) => HomePage(),
-    //   '/livelocation': (context) => MapPage()
-    // },
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/login': (context) => Login(),
+      //   '/register': (context) => Signup(),
+      //   '/homepage': (context) => HomePage(),
+      //   '/livelocation': (context) => MapPage()
+    },
     home: new MyApp(),
   ));
 }
@@ -27,19 +27,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Text('Hello!');
-
-    // return new SplashScreen(
-    //     seconds: 1,
-    //     // navigateAfterSeconds: new Login(),
-    //     title: new Text(
-    //       'Welcome to BusTracker',
-    //       style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-    //     ),
-    //     // image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
-    //     backgroundColor: Color(0xff2a9d8f),
-    //     styleTextUnderTheLoader: new TextStyle(),
-    //     photoSize: 100.0,
-    //     loaderColor: Colors.red);
+    return new SplashScreen(
+        seconds: 1,
+        navigateAfterSeconds: new Login(),
+        title: new Text(
+          'Welcome to BusTracker',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        // image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Color(0xff2a9d8f),
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.red);
   }
 }
