3e2a3ffafad7fef7e79ecae9af889a1a085595f3
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-04-25 09:37:23 +0800

    Clean code

--- front-end-mobile/lib/main.dart
+++ front-end-mobile/lib/main.dart
@@ -7,36 +7,38 @@ import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
       '/login': (context) => Login(),
       //   '/register': (context) => Signup(),
-      '/homepage': (context) => HomePage(),
+      '/homepage': (context) => const HomePage(),
       //   '/livelocation': (context) => MapPage()
     },
-    home: new MyApp(),
+    home: MyApp(),
   ));
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-        seconds: 1,
-        navigateAfterSeconds: new HomePage(),
-        title: new Text(
-          'Welcome to BusTracker',
-          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-        ),
-        // image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
-        backgroundColor: Color(0xff2a9d8f),
-        styleTextUnderTheLoader: new TextStyle(),
+    return SplashScreen(
+        seconds: 4,
+        navigateAfterSeconds: HomePage(),
+        // title: new Text(
+        //   'Welcome to BusTracker',
+        //   style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        // ),
+        image: Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: const Color(0xff2a9d8f),
+        styleTextUnderTheLoader: const TextStyle(),
         photoSize: 100.0,
         loaderColor: Colors.red);
   }
