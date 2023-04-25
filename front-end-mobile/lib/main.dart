b9e6d0c73dd036826ce37983dfeb449d89088013
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-04-25 09:45:42 +0800

    Import com logo image

--- front-end-mobile/lib/main.dart
+++ front-end-mobile/lib/main.dart
@@ -31,12 +31,16 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return SplashScreen(
         seconds: 4,
-        navigateAfterSeconds: HomePage(),
+        navigateAfterSeconds: const HomePage(),
         // title: new Text(
         //   'Welcome to BusTracker',
         //   style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         // ),
-        image: Image.network('https://i.imgur.com/TyCSG9A.png'),
+        image: const Image(
+          image: AssetImage('assets/images/com-02.png'),
+          width: 300,
+          height: 300,
+        ),
         backgroundColor: const Color(0xff2a9d8f),
         styleTextUnderTheLoader: const TextStyle(),
         photoSize: 100.0,
