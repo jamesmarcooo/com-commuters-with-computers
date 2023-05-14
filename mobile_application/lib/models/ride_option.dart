3ed3a883e82f0722374a6126bc40756d98975249
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-05-15 01:51:04 +0800

    Add models

---
+++ mobile_application/lib/models/ride_option.dart
@@ -0,0 +1,12 @@
+class RideOption {
+  final String title;
+  final Duration timeOfArrival;
+  final double price;
+  final String icon;
+
+  const RideOption(
+      {required this.title,
+      required this.timeOfArrival,
+      required this.price,
+      required this.icon});
+}
