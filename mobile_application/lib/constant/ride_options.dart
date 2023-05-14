eb8303307cefe2bb7ce474cb98efffe638456ac0
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-05-15 01:49:34 +0800

    Add constants

---
+++ mobile_application/lib/constant/ride_options.dart
@@ -0,0 +1,23 @@
+import 'package:mobile_application/models/ride_option.dart';
+import 'package:mobile_application/utils/icons_assets.dart';
+
+const List<RideOption> rideOptions = const [
+  const RideOption(
+    title: 'Stardard',
+    timeOfArrival: const Duration(minutes: 5),
+    price: 500,
+    icon: IconsAssets.stardard_car,
+  ),
+  const RideOption(
+    title: 'Premium',
+    timeOfArrival: const Duration(minutes: 3),
+    price: 1000,
+    icon: IconsAssets.premium_car,
+  ),
+  const RideOption(
+    title: 'VIP',
+    timeOfArrival: const Duration(minutes: 2),
+    price: 2000,
+    icon: IconsAssets.vip_car,
+  ),
+];
