36b5a234c31ff216d05dcc14b9e59025c5c05fea
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-06-07 16:17:42 +0800

    Create firebase repository

--- mobile_application/lib/models/bus.dart
+++ mobile_application/lib/models/bus.dart
@@ -18,7 +18,7 @@ class Bus {
   factory Bus.fromMap(Map<String, dynamic> data) {
     return Bus(
       id: data['id'] ?? '',
-      busList: List<Eta>.from(data['bus'] ?? []),
+      busList: List<Eta>.from(data['busList'] ?? []),
       driverUID: data['driver_uid'] ?? '',
       startAddress: Address.fromMap(data['start_address'] ?? {}),
       endAddress: Address.fromMap(data['end_address'] ?? {}),
---
+++ mobile_application/lib/repositories/bus_repository.dart
@@ -0,0 +1,105 @@
+import 'package:mobile_application/models/bus.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class BusRepository {
+  BusRepository._();
+  static BusRepository? _instance;
+
+  static BusRepository get instance {
+    _instance ??= BusRepository._();
+    return _instance!;
+  }
+
+  final _firestoreBusCollection =
+      FirebaseFirestore.instance.collection('buses');
+
+  ValueNotifier<List<Bus>> busNotifier = ValueNotifier<List<Bus>>([]);
+  List<Bus> get busList => busNotifier.value;
+
+  Future<Bus?> loadRide(String id) async {
+    try {
+      final ride = busList.firstWhere((Bus ride) => ride.id == id);
+      return ride;
+    } catch (_) {
+      try {
+        final doc = await _firestoreBusCollection.doc(id).get();
+        final ride = Bus.fromMap(doc.data() ?? {});
+        busNotifier.value.add(ride);
+        busNotifier.notifyListeners();
+
+        return ride;
+      } on FirebaseException catch (e) {
+        print(e.message);
+        return null;
+      }
+    }
+  }
+
+  Future<List<Bus>> loadAllBus(String ownerUID) async {
+    try {
+      final snapshot = _firestoreBusCollection
+          .where('owner_uid', isEqualTo: ownerUID)
+          .snapshots();
+      snapshot.listen((query) {
+        _addToBusList(query);
+      });
+      return busList;
+    } on FirebaseException catch (_) {
+      print('something occurred');
+      return busList;
+    }
+  }
+
+  void _addToBusList(QuerySnapshot<Map<String, dynamic>> query) {
+    Future.wait(query.docs.map((doc) async {
+      final ride = Bus.fromMap(doc.data());
+      final index = busList.indexWhere((rideX) => rideX.id == ride.id);
+      if (index != -1) {
+        busNotifier.value.removeAt(index);
+        busNotifier.value.insert(index, ride);
+      } else {
+        busNotifier.value.add(ride);
+      }
+      busNotifier.notifyListeners();
+    }));
+  }
+
+  Future<Bus?> boardRide(Bus bus) async {
+    try {
+      final startAddress = bus.startAddress;
+      final endAddress = bus.endAddress;
+
+      await _firestoreBusCollection.doc(bus.id).set({
+        'id': bus.id,
+        'busList': bus.busList,
+        'driverUID': bus.driverUID,
+        'start_address': {
+          'city': startAddress.city,
+          'country': startAddress.country,
+          'latlng': {
+            'lat': startAddress.latLng.latitude,
+            'lng': startAddress.latLng.longitude,
+          },
+          'post_code': startAddress.postcode,
+          'state': startAddress.state,
+        },
+        'end_address': {
+          'city': endAddress.city,
+          'country': endAddress.country,
+          'latlng': {
+            'lat': endAddress.latLng.latitude,
+            'lng': endAddress.latLng.longitude,
+          },
+          'post_code': endAddress.postcode,
+          'state': endAddress.state,
+        },
+      });
+      final addedRide = await loadRide(bus.id);
+      return addedRide;
+    } on FirebaseException catch (_) {
+      print('could not board ride');
+      return null;
+    }
+  }
+}
