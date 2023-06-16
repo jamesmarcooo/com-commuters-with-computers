94fbeacb46e58876cf5704105af7aebcb812b76f
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-06-17 01:25:00 +0800

    Update eta backend

--- mobile_application/lib/models/eta.dart
+++ mobile_application/lib/models/eta.dart
@@ -3,14 +3,16 @@ import 'package:mobile_application/models/user.dart';
 class Eta {
   // final User driver;
   final Map<String, dynamic> driver;
-  final int eta;
+  final int etaStartBus;
+  final int etaEndBus;
   final DateTime timeOfArrival;
   final double distanceStartBus;
   final double distanceEndBus;
 
   const Eta(
       {required this.driver,
-      required this.eta,
+      required this.etaStartBus,
+      required this.etaEndBus,
       required this.timeOfArrival,
       required this.distanceStartBus,
       required this.distanceEndBus});
@@ -18,7 +20,8 @@ class Eta {
   factory Eta.fromMap(Map<String, dynamic> data) {
     return Eta(
       driver: data['driver'] ?? {},
-      eta: data['eta'],
+      etaStartBus: data['etaStartBus'],
+      etaEndBus: data['etaEndBus'],
       timeOfArrival: DateTime.fromMillisecondsSinceEpoch(
           data['timeOfArrival'].millisecondsSinceEpoch),
       distanceStartBus: data['distanceStartBus'],
